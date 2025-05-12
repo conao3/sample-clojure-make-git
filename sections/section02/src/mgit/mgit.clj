edb098832cc4cc585e5a3fa89056b4c7f476eb09
conao3 <conao3@gmail.com>
2025-05-13 08:09:53 +0900

    commit

--- sections/section02/src/mgit/mgit.clj
+++ sections/section02/src/mgit/mgit.clj
@@ -1,5 +1,6 @@
 (ns mgit.mgit
   (:require
+   [clojure.pprint :as pprint]
    [clojure.string :as str]
    [clj-commons.digest :as digest]
    [babashka.fs :as fs])
@@ -40,19 +41,6 @@
             (recur))))
       (. inflater end)
       (. byte-stream toByteArray))))
-
-(defn read-uint32 [^bytes data offset]
-  (bit-or (bit-shift-left (bit-and (aget data offset) 0xff) 24)
-          (bit-shift-left (bit-and (aget data (+ offset 1)) 0xff) 16)
-          (bit-shift-left (bit-and (aget data (+ offset 2)) 0xff) 8)
-          (bit-and (aget data (+ offset 3)) 0xff)))
-
-(defn read-uint16 [^bytes data offset]
-  (bit-or (bit-shift-left (bit-and (aget data offset) 0xff) 8)
-          (bit-and (aget data (+ offset 1)) 0xff)))
-
-(defn read-string [^bytes data offset len]
-  (String. data offset len "UTF-8"))
 
 (defn parse [handler args]
   (reduce
@@ -140,202 +128,85 @@
         (. out write blob (inc sepinx) (- (alength blob) (inc sepinx)))
         (throw (ex-info "fatal: Required `-p' option" {:args args}))))))
 
-(defn parse-index-entry [^bytes data offset]
-  (let [ctime-sec (read-uint32 data offset)
-        ctime-nsec (read-uint32 data (+ offset 4))
-        mtime-sec (read-uint32 data (+ offset 8))
-        mtime-nsec (read-uint32 data (+ offset 12))
-        dev (read-uint32 data (+ offset 16))
-        ino (read-uint32 data (+ offset 20))
-        mode (read-uint32 data (+ offset 24))
-        uid (read-uint32 data (+ offset 28))
-        gid (read-uint32 data (+ offset 32))
-        file-size (read-uint32 data (+ offset 36))
-        sha1 (format "%040x"
-                    (BigInteger. 1 (java.util.Arrays/copyOfRange data
-                                                                (+ offset 40)
-                                                                (+ offset 60))))
-        flags (read-uint16 data (+ offset 60))
-        name-len (bit-and flags 0xfff)
-        name-offset (+ offset 62)
-        name (read-string data name-offset name-len)
-        entry-size (+ 62 name-len (- 8 (mod (+ 62 name-len) 8)))]
-    {:entry {:ctime-sec ctime-sec
-             :ctime-nsec ctime-nsec
-             :mtime-sec mtime-sec
-             :mtime-nsec mtime-nsec
-             :dev dev
-             :ino ino
-             :mode mode
-             :uid uid
-             :gid gid
-             :file-size file-size
-             :sha1 sha1
-             :flags flags
-             :name name}
-     :entry-size entry-size}))
-
 (defn parse-index [^bytes data]
-  (let [header (read-string data 0 4)]
-    (when-not (= header "DIRC")
-      (throw (ex-info "Invalid index file format" {:header header})))
+  (let [bytes->int #(reduce (fn [acc elm] (+ (bit-shift-left acc 8) (bit-and elm 0xff))) 0 %)
+        bytes->str #(-> % byte-array String.)
+        bytes->hexstr #(->> % ((partial map (partial format "%02x"))) (apply str))
+        parse (fn [acc key parse-fn remaining-update-fn]
+                (-> acc
+                    (assoc-in [:parsed key] (parse-fn acc))
+                    (update :remaining remaining-update-fn)))
+        parse-bytes (fn [acc key size & [update-value-fn]]
+                      (-> acc
+                          (parse key #(take size (:remaining %)) (partial drop size))
+                          (update-in [:parsed key] (or update-value-fn identity))))
+        parse-int (fn [acc key size]
+                    (-> acc
+                        (parse-bytes key size bytes->int)))
+        parse-uint16 (fn [acc key] (parse-int acc key 2))
+        parse-uint32 (fn [acc key] (parse-int acc key 4))
+        parse-dirc (fn [acc key]
+                     (parse acc
+                            key
+                            #(do
+                               (when-not (= (take 4 (:remaining %))
+                                            (seq (. "DIRC" getBytes)))
+                                 (throw (ex-info "Data should be start with `DIRC'" {:acc %})))
+                               true)
+                            (partial drop 4)))
+        parse-filepath (fn [acc key]
+                         (let [res (loop [bytes []
+                                          find-null false
+                                          remaining (:remaining acc)]
+                                     (let [d (first remaining)]
+                                       (cond
+                                         (= d 0)
+                                         (recur bytes true (rest remaining))
 
-    (let [version (read-uint32 data 4)
-          entries-count (read-uint32 data 8)
-          entries (loop [offset 12
-                         entries []
-                         remaining entries-count]
-                    (if (zero? remaining)
-                      entries
-                      (let [{:keys [entry entry-size]} (parse-index-entry data offset)]
-                        (recur (+ offset entry-size)
-                               (conj entries entry)
-                               (dec remaining)))))]
-      {:version version
-       :entries-count entries-count
-       :entries entries})))
+                                         (and (not= d 0) (not find-null))
+                                         (recur (conj bytes d) false (rest remaining))
 
-(defn create-index-entry [file-path hash stat]
-  (let [stat-dev (or (:dev stat) 0)
-        stat-ino (or (:ino stat) 0)
-        stat-mode (or (:mode stat) 0)
-        stat-uid (or (:uid stat) 0)
-        stat-gid (or (:gid stat) 0)
-        stat-size (or (:size stat) 0)
-        ctime (.toEpochSecond (or (:ctime stat) (java.time.Instant/now)))
-        ctime-nsec 0
-        mtime (.toEpochSecond (or (:mtime stat) (java.time.Instant/now)))
-        mtime-nsec 0
-        name (str file-path)]
-    {:ctime-sec ctime
-     :ctime-nsec ctime-nsec
-     :mtime-sec mtime
-     :mtime-nsec mtime-nsec
-     :dev stat-dev
-     :ino stat-ino
-     :mode stat-mode
-     :uid stat-uid
-     :gid stat-gid
-     :file-size stat-size
-     :sha1 hash
-     :flags (count name)
-     :name name}))
+                                         (and (not= d 0) find-null)
+                                         {:bytes bytes :remaining remaining})))]
+                           (-> acc
+                               (assoc-in [:parsed key] (bytes->str (:bytes res)))
+                               (assoc :remaining (:remaining res)))))
+        parse-entry (fn [acc key]
+                      (let [res (-> (assoc acc :parsed {})
+                                    (parse-uint32 :ctime-sec)
+                                    (parse-uint32 :ctime-nsec)
+                                    (parse-uint32 :mtime-sec)
+                                    (parse-uint32 :mtime-nsec)
+                                    (parse-uint32 :dev)
+                                    (parse-uint32 :ino)
+                                    (parse-uint32 :mode)
+                                    (parse-uint32 :uid)
+                                    (parse-uint32 :gid)
+                                    (parse-uint32 :size)
+                                    (parse-bytes :object-id 20 bytes->hexstr)
+                                    (parse-uint16 :flag)
+                                    (parse-filepath :filepath))]
+                        (-> acc
+                            (update-in [:parsed key] #(or % []))
+                            (update-in [:parsed key] conj (:parsed res))
+                            (assoc :remaining (:remaining res)))))
+        parse-entries (fn [acc key]
+                        (nth (iterate #(parse-entry % key) acc)
+                             (-> acc :parsed :number-of-entries)))]
+    (-> {:parsed {} :remaining (seq data)}
+        (parse-dirc :dirc)
+        (parse-uint32 :version)
+        (parse-uint32 :number-of-entries)
+        (parse-entries :entries)
+        (parse-bytes :checksum 20 bytes->hexstr))))
 
-(defn write-uint32 [baos value]
-  (. baos write (bit-and (bit-shift-right value 24) 0xff))
-  (. baos write (bit-and (bit-shift-right value 16) 0xff))
-  (. baos write (bit-and (bit-shift-right value 8) 0xff))
-  (. baos write (bit-and value 0xff)))
-
-(defn write-uint16 [baos value]
-  (. baos write (bit-and (bit-shift-right value 8) 0xff))
-  (. baos write (bit-and value 0xff)))
-
-(defn write-index [index]
-  (with-open [baos (ByteArrayOutputStream.)]
-    (let [entries-count (count (:entries index))]
-      ;; Write header
-      (. baos write (. "DIRC" getBytes "UTF-8"))
-      (write-uint32 baos (:version index))
-      (write-uint32 baos entries-count)
-
-      ;; Write entries
-      (doseq [entry (sort-by :name (:entries index))]
-        (write-uint32 baos (:ctime-sec entry))
-        (write-uint32 baos (:ctime-nsec entry))
-        (write-uint32 baos (:mtime-sec entry))
-        (write-uint32 baos (:mtime-nsec entry))
-        (write-uint32 baos (:dev entry))
-        (write-uint32 baos (:ino entry))
-        (write-uint32 baos (:mode entry))
-        (write-uint32 baos (:uid entry))
-        (write-uint32 baos (:gid entry))
-        (write-uint32 baos (:file-size entry))
-
-        ;; Write SHA-1
-        (let [sha1-bytes (. (BigInteger. (:sha1 entry) 16) toByteArray)]
-          (. baos write (byte-array (- 20 (alength sha1-bytes))))
-          (. baos write sha1-bytes))
-
-        ;; Write flags & name
-        (let [name (:name entry)
-              name-len (count name)]
-          (write-uint16 baos (bit-and name-len 0xfff))
-          (. baos write (. name getBytes "UTF-8"))
-
-          ;; Padding to multiple of 8 bytes
-          (let [padding-size (- 8 (mod (+ 62 name-len) 8))]
-            (when (< padding-size 8)
-              (. baos write (byte-array padding-size))))))
-
-      ;; Return the index data
-      (. baos toByteArray))))
-
-(defn cmd-update-index
-  "Register file into working tree"
-  [& args]
-  (let [{:keys [options error]}
-        (->> (rest args)
-             (parse
-              (fn [acc]
-                (condp apply [(-> acc :remaining first)]
-                  #{"--add"} (-> acc
-                                (assoc-in [:options :add] true)
-                                (update :remaining next))
-                  nil))))]
-    (when error
-      (throw (ex-info error {:args args})))
-
-    (when-not (:add options)
-      (throw (ex-info "fatal: Specify `--add' option" {:args args})))
-
-    (let [index-file (fs/file git-dir "index")
-          index (if (fs/exists? index-file)
-                  (-> index-file
-                      fs/read-all-bytes
-                      parse-index)
-                  {:version 2
-                   :entries-count 0
-                   :entries []})
-          entries (:entries index)]
-
-      (doseq [file-path (:args options)]
-        (let [file (fs/file file-path)
-              content (fs/read-all-bytes file)
-              stat (fs/file-attributes file)
-              ;; Create blob object
-              blob-content (with-open [byte-stream (ByteArrayOutputStream.)
-                                       writer (OutputStreamWriter. byte-stream "UTF-8")]
-                             (. writer write (format "blob %s\0" (count content)))
-                             (. writer flush)
-                             (. byte-stream write content)
-                             (. byte-stream toByteArray))
-              hash (digest/sha1 blob-content)
-              dir-name (subs hash 0 2)
-              file-name (subs hash 2)
-              file-path-in-objects (fs/file objects-dir dir-name file-name)]
-
-          ;; Store blob
-          (when-not (fs/exists? file-path-in-objects)
-            (fs/create-dirs (fs/parent file-path-in-objects))
-            (fs/write-bytes file-path-in-objects (zlib-compress blob-content)))
-
-          ;; Create index entry
-          (let [entry (create-index-entry file-path hash stat)
-                existing-entry-index (when entries
-                                       (first (keep-indexed
-                                               (fn [idx itm] (when (= (:name itm) file-path) idx))
-                                               entries)))
-                new-entries (if existing-entry-index
-                              (assoc entries existing-entry-index entry)
-                              (conj (or entries []) entry))
-                new-index {:version (:version index)
-                          :entries-count (count new-entries)
-                          :entries new-entries}]
-
-            ;; Write index file
-            (fs/write-bytes index-file (write-index new-index))
-            (println (format "added '%s'" file-path))))))))
+(defn cmd-parse-index
+  "Parse .git/index"
+  [& _args]
+  (-> (fs/file git-dir "index")
+      fs/read-all-bytes
+      parse-index
+      pprint/pprint))
 
 (defn cmd-init
   "Initialize git repository"
@@ -360,7 +231,7 @@
 (def actions {"init" #'cmd-init
               "hash-object" #'cmd-hash-object
               "cat-file" #'cmd-cat-file
-              "update-index" #'cmd-update-index
+              "parse-index" #'cmd-parse-index
               "help" #'cmd-help})
 
 (defn -main [& args]
