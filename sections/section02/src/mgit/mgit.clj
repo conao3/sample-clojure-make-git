f2cca57ef04d5c97b7855b331827f1f666555dcb
conao3 <conao3@gmail.com>
2025-05-17 18:09:53 +0900

    add help

--- sections/section02/src/mgit/mgit.clj
+++ sections/section02/src/mgit/mgit.clj
@@ -1,5 +1,28 @@
 (ns mgit.mgit
   (:gen-class))
 
+(declare actions)
+
+(defn cmd-init
+  "Initialize git repository"
+  [& args]
+  (println args))
+
+(defn cmd-help
+  "Help"
+  [& _args]
+  (println "Available actions:")
+  (doseq [[k v] actions]
+    (println (format "  %s - %s" k (:doc (meta v))))))
+
+(def actions {"init" #'cmd-init
+              "help" #'cmd-help})
+
 (defn -main [& args]
-  (println "Hello World," args))
+  (let [action (get actions (first args))]
+    (if action
+      (apply action args)
+      (do
+        (when (first args)
+          (println (format "`%s` is not a mgit action.\n" (first args))))
+        (cmd-help args)))))
