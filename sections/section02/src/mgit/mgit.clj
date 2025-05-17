31b35f15757a29a6b3674412d64a1f9cac0c2f9a
conao3 <conao3@gmail.com>
2025-05-17 21:28:35 +0900

    add parse-index function and ls-files command

--- sections/section02/src/mgit/mgit.clj
+++ sections/section02/src/mgit/mgit.clj
@@ -14,6 +14,78 @@
   (binding [*out* *err*]
     (apply println args)))
 
+(defn parse-index [^bytes data]
+  (let [bytes->int #(reduce (fn [acc elm] (+ (bit-shift-left acc 8) (bit-and elm 0xff))) 0 %)
+        bytes->str #(-> % byte-array String.)
+        bytes->hexstr #(->> % ((partial map (partial format "%02x"))) (apply str))
+        parse (fn [acc key parse-fn remaining-update-fn]
+                (-> acc
+                    (assoc-in [:parsed key] (parse-fn acc))
+                    (update :remaining remaining-update-fn)))
+        parse-bytes (fn [acc key size & [update-value-fn]]
+                      (-> acc
+                          (parse key #(take size (:remaining %)) (partial drop size))
+                          (update-in [:parsed key] (or update-value-fn identity))))
+        parse-int (fn [acc key size]
+                    (-> acc
+                        (parse-bytes key size bytes->int)))
+        parse-uint16 (fn [acc key] (parse-int acc key 2))
+        parse-uint32 (fn [acc key] (parse-int acc key 4))
+        parse-dirc (fn [acc key]
+                     (parse acc
+                            key
+                            #(do
+                               (when-not (= (take 4 (:remaining %))
+                                            (seq (. "DIRC" getBytes)))
+                                 (throw (ex-info "Data should be start with `DIRC'" {:acc %})))
+                               true)
+                            (partial drop 4)))
+        parse-filepath (fn [acc key]
+                         (let [res (loop [bytes []
+                                          find-null false
+                                          remaining (:remaining acc)]
+                                     (let [d (first remaining)]
+                                       (cond
+                                         (= d 0)
+                                         (recur bytes true (rest remaining))
+
+                                         (and (not= d 0) (not find-null))
+                                         (recur (conj bytes d) false (rest remaining))
+
+                                         (and (not= d 0) find-null)
+                                         {:bytes bytes :remaining remaining})))]
+                           (-> acc
+                               (assoc-in [:parsed key] (bytes->str (:bytes res)))
+                               (assoc :remaining (:remaining res)))))
+        parse-entry (fn [acc key]
+                      (let [res (-> (assoc acc :parsed {})
+                                    (parse-uint32 :ctime-sec)
+                                    (parse-uint32 :ctime-nsec)
+                                    (parse-uint32 :mtime-sec)
+                                    (parse-uint32 :mtime-nsec)
+                                    (parse-uint32 :dev)
+                                    (parse-uint32 :ino)
+                                    (parse-uint32 :mode)
+                                    (parse-uint32 :uid)
+                                    (parse-uint32 :gid)
+                                    (parse-uint32 :size)
+                                    (parse-bytes :object-id 20 bytes->hexstr)
+                                    (parse-uint16 :flag)
+                                    (parse-filepath :filepath))]
+                        (-> acc
+                            (update-in [:parsed key] #(or % []))
+                            (update-in [:parsed key] conj (:parsed res))
+                            (assoc :remaining (:remaining res)))))
+        parse-entries (fn [acc key]
+                        (nth (iterate #(parse-entry % key) acc)
+                             (-> acc :parsed :number-of-entries)))]
+    (-> {:parsed {} :remaining (seq data)}
+        (parse-dirc :dirc)
+        (parse-uint32 :version)
+        (parse-uint32 :number-of-entries)
+        (parse-entries :entries)
+        (parse-bytes :checksum 20 bytes->hexstr))))
+
 (defn parse [handler args]
   (reduce
    (fn [acc _]
@@ -35,6 +107,18 @@
    {:options {:args []} :remaining args}
    args))
 
+(defn cmd-init
+  "Initialize git repository"
+  [& _args]
+  (doseq [dir [git-dir objects-dir refs-dir]]
+    (-> dir fs/create-dirs))
+
+  (->> "ref: refs/heads/master\n"
+       (spit (fs/file git-dir "HEAD")))
+
+  (-> (format "Initialized empty Git repository in %s" (-> git-dir fs/absolutize))
+      eprintln))
+
 (defn cmd-add
   "Add file contents to the index"
   [& args]
@@ -48,17 +132,15 @@
                          nil))))]
     (eprintln parsed)))
 
-(defn cmd-init
-  "Initialize git repository"
+(require 'clojure.pprint)
+
+(defn cmd-ls-files
+  "Parse .git/index"
   [& _args]
-  (doseq [dir [git-dir objects-dir refs-dir]]
-    (-> dir fs/create-dirs))
-
-  (->> "ref: refs/heads/master\n"
-       (spit (fs/file git-dir "HEAD")))
-
-  (-> (format "Initialized empty Git repository in %s" (-> git-dir fs/absolutize))
-      eprintln))
+  (-> (fs/file git-dir "index")
+      fs/read-all-bytes
+      parse-index
+      clojure.pprint/pprint))
 
 (defn cmd-help
   "Help"
@@ -70,6 +152,7 @@
 
 (def actions {"init" #'cmd-init
               "add" #'cmd-add
+              "ls-files" #'cmd-ls-files
               "help" #'cmd-help})
 
 (defn -main [& args]
