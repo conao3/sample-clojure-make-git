f2992b1ca3d6b27add547f7add5d7a2cbf922686
conao3 <conao3@gmail.com>
2025-05-17 18:41:25 +0900

    fmt

--- flake.nix
+++ flake.nix
@@ -10,7 +10,10 @@
   outputs =
     inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
 
       imports = [
         inputs.treefmt-nix.flakeModule
